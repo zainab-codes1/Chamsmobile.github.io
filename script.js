fe919749e2bab326efacc56cea43e71eb214a76a
zainab-codes1 <157022676+zainab-codes1@users.noreply.github.com>
2024-05-17 10:27:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function getStarted(){
+    window.location.href =
+    "https://kegow.com/#download"
+}
+
+//document.addEventListener('keydown',
+//function(event){
+//        if (event.key === 'Enter'){
+//            getStarted();
+//        }
+//    }
+//);
